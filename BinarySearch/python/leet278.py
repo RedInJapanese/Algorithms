86b5714b821b0b12828069268f152b5163931729
RedInJapanese <abasu924@gmail.com>
2022-06-19 00:26:48 +0000

    debug conditional

--- BinarySearch/python/leet278.py
+++ BinarySearch/python/leet278.py
@@ -4,7 +4,7 @@
     while(L<=R):
         pivot = L + (R-L)//2
         if(isBadVersion(pivot)):
-            if(isBadVersion(pivot-1)):
+            if(isBadVersion(pivot-1) == False):
                 return pivot
             else: 
                 R = pivot - 1
