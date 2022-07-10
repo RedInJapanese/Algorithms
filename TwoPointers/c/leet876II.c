bd2e43a8491a8eaeac6dbd22bd4d990ba131857a
RedInJapanese <abasu924@gmail.com>
2022-07-10 15:00:35 +0000

    add tortoise and hare implementation for leetcode #876

---
+++ TwoPointers/c/leet876II.c
@@ -0,0 +1,27 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h>
+
+#define MAX 101
+
+struct ListNode* middleNode(struct ListNode* head);
+
+struct ListNode {
+    int val; 
+    struct ListNode *next;
+};
+
+int main(void){
+    puts("test");
+}
+
+struct ListNode* middleNode(struct ListNode* head){
+    struct ListNode* fast = head; 
+    struct ListNode* slow = head; 
+    
+    while(fast && fast->next){
+        slow = slow->next; 
+        fast = fast->next->next;
+    }
+    return slow;
+}   
